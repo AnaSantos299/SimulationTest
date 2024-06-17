614a29f4969b4e80ede17cf355814f5fe366ee18
AnaSantos299 <catarinaantunesajp@gmail.com>
2024-06-17 17:01:37 +0100

    Scene 2 subtitles completed
    Scene 2 subtitles completed, scene 3 subtitles started, half done

--- DreamScape/Assets/Scene2Events.cs
+++ DreamScape/Assets/Scene2Events.cs
@@ -2,42 +2,81 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Scene2Events : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public DialogueRunner dialogueRunner;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject phone; //Image from the phone
+    public GameObject phoneOpenMessage; //Image from the phone showing the messages
+    public GameObject phoneApps; //Image showing the apps on the phone
+    public GameObject phoneBank; //Image showing the bank account
 
- public DialogueRunner dialogueRunner;
+    public GameObject returnHomeButton; //Button to return home
 
+    public List<Sprite> images; // Array to hold all images from returningHome
+    public Image displayImage; //The UI element where the images will be displayed
 
-    public void SayHello(){
-                dialogueRunner.Stop();
-        //jumpt to node acceptCall
-        dialogueRunner.StartDialogue("SayHelloNode");
-    }
-
-    public GameObject phone;
-
-[YarnCommand("makePhoneVisible")]
+    //Show the initial Phone image
+    [YarnCommand("makePhoneVisible")]
     public void MakePhoneVisible()
     {
 
         phone.SetActive(true);
     }
 
-[YarnCommand("makePhoneInvisible")]
-    public void MakePhoneInvisible()
+    //Show PhoneOpenMessage Image and Hide initial phone image
+    [YarnCommand("makePhoneOpenMessageVisible")]
+    public void MakePhoneOpenMessageVisible()
     {
         phone.SetActive(false);
+        phoneOpenMessage.SetActive(true);
     }
+
+    //Show PhoneApps and Hide PhoneOpenMessage Image
+    [YarnCommand("makePhoneAppsVisible")]
+    public void MakePhoneAppsVisible()
+    {
+        phoneOpenMessage.SetActive(false);
+        phoneApps.SetActive(true);
+    }
+
+    //----------------------------------BUTTONS----------------------------------------
+    public void OpenBankApp()
+    {
+        //Stop previous dialogue
+        dialogueRunner.Stop();
+        //Start OpenBankApp Dialogue
+        dialogueRunner.StartDialogue("OpenBankApp");
+        phoneApps.SetActive(false);
+        phoneBank.SetActive(true);
+
+    }
+    //
+    [YarnCommand("returnHome")]
+    public void ReturnHome()
+    {
+        phoneBank.SetActive(false);
+        StartCoroutine(DisplayImages());
+    }
+
+    //----------------------------------COROUTINES----------------------------------------
+
+    private IEnumerator DisplayImages()
+    {
+        returnHomeButton.SetActive(false); //Disactivate the returnHome Button
+        dialogueRunner.Stop(); //Stop Dialogue
+        Debug.Log("Entrou na Coroutine");
+        foreach (Sprite image in images) //For each image, display one at a time with the interval of 3 seconds
+        {
+            displayImage.sprite = image; // Set the current image
+            yield return new WaitForSeconds(3f); // Wait for 3s
+        }
+        //Chance scene after finishing showing all the images
+        Debug.Log("Mudança de scene");
+        SceneManager.LoadScene("Scene3");
+    }
+
 }
