8a2d8cf5c48ab7af0362fa2e81b7e36dbea6e0d7
AnaSantos299 <catarinaantunesajp@gmail.com>
2024-06-18 16:58:11 +0100

    Scene 3 subtitles problem
    Problema com as legendas, yarn ignora do ficheiro da scene 3

---
+++ DreamScape/Assets/Scene3Events.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Yarn.Unity;
+using UnityEngine.UI;
+
+public class Scene3Events : MonoBehaviour
+{
+    //Dialogue
+    public DialogueRunner dialogueRunner; // dialogue runner
+    public GameObject[] objectsicons; // array of the icons of the objects, this icons appear on the right side of the screen
+    private int objectCount; //variable to keep track of how many icons are visible (this means how many objects the player have found in the room)
+
+
+    public List<Sprite> images; // Array to hold all images from returningHome
+    public Image displayImage; //The UI element where the images will be displayed
+    public GameObject displayImageObj; //Temporary to turn the gameobject active
+
+    //----------------------------------SHOW/HIDE IMAGES----------------------------------------
+    [YarnCommand("transition_Scene4Images")]
+    public void Transition_Scene4Images()
+    {
+        //coroutine for the transaction from Scene3 to Scene4
+        StartCoroutine(DisplayImagesForTransition());
+    }
+
+    //----------------------------------VERIFICATION----------------------------------------
+    [YarnCommand("checkObjectIconsCount")]
+    public void CheckObjectIconsCount()
+    {
+        //if the objectcount is >= 3, it will call the dialogue from the Scene3_EN.yarn and start the transition to the next scene.
+        if (objectCount >= 3)
+        {
+            Debug.Log("OBJECT COUNT IS 3");
+            //stop the dialogue running
+            dialogueRunner.Stop();
+            //Start new dialogue
+            dialogueRunner.StartDialogue("transition_Scene4");
+        }
+    }
+
+    //----------------------------------ACTIONS FROM CLICKS----------------------------------------
+    public void TableObject()
+    {
+        //stop the dialogue running
+        dialogueRunner.Stop();
+        //Start new dialogue
+        dialogueRunner.StartDialogue("tableObject");
+        //show the first object of the array objectsicons
+        objectsicons[0].SetActive(true);
+        //add one to the object Count
+        objectCount++;
+        Debug.Log(objectCount);
+    }
+
+    public void CabinetObject()
+    {
+        //stop the dialogue running
+        dialogueRunner.Stop();
+        //Start new dialogue
+        dialogueRunner.StartDialogue("cabinetObject");
+        //show the second object of the array objectsicons
+        objectsicons[1].SetActive(true);
+        //add one to the object count
+        objectCount++;
+        Debug.Log(objectCount);
+    }
+
+    public void BedObject()
+    {
+        //stop the dialogue running
+        dialogueRunner.Stop();
+        //Start new dialogue
+        dialogueRunner.StartDialogue("bedObject");
+        //show the third object of the array objectsicons
+        objectsicons[2].SetActive(true);
+        //add one to the object count
+        objectCount++;
+        Debug.Log(objectCount);
+    }
+
+    //----------------------------------COROUTINES----------------------------------------
+
+    private IEnumerator DisplayImagesForTransition()
+    {
+        //Turn the gameobject displayimage active true
+        displayImageObj.SetActive(true);
+        Debug.Log("Entrou na Coroutine");
+        foreach (Sprite image in images) //For each image, display one at a time with the interval of 3 seconds
+        {
+            Debug.Log("Sprite" + image);
+            displayImage.sprite = image; // Set the current image
+            yield return new WaitForSeconds(3f); // Wait for 3s
+        }
+        //Chance scene after finishing showing all the images
+        Debug.Log("Mudança de scene");
+        SceneManager.LoadScene("Scene4");
+    }
+
+}
---
+++ DreamScape/Assets/Scripts/Scene3/InputHandle.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class InputHandle : MonoBehaviour
+{
+    // Reference to the Scene3Events script
+    public Scene3Events scene3Events;
+
+    // Name of the method to call on click
+    public string methodName;
+
+#if UNITY_WEBGL
+    // This method is called when the user releases the mouse button over the Collider
+    void OnMouseClick()
+    {
+        InvokeMethod();
+    }
+
+    // Handles mouse input in WebGL and standalone builds
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform == transform)
+                {
+                    OnMouseClick(); // Reuse the same logic for mouse clicks
+                }
+            }
+        }
+    }
+#endif
+
+#if UNITY_IOS || UNITY_ANDROID
+    // Handles touch input on mobile devices
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Ended)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.transform == transform)
+                    {
+                        InvokeMethod(); // Call the method directly on touch
+                    }
+                }
+            }
+        }
+    }
+#endif
+
+    // Method to invoke the assigned method on Scene3Events
+    private void InvokeMethod()
+    {
+        var method = scene3Events.GetType().GetMethod(methodName);
+        if (method != null)
+        {
+            method.Invoke(scene3Events, null);
+        }
+        else
+        {
+            Debug.LogWarning("Method " + methodName + " not found on Scene3Events");
+        }
+    }
+}
